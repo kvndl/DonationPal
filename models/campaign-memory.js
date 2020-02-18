b71a774fa7503ae723569e9015f0683c2c0e4438
Noah SChepers <noaschep@iu.edu>
2020-02-18 17:38:39 -0500

    adding memory to campaigns

---
+++ models/campaign-memory.js
@@ -0,0 +1,18 @@
+const Campaign = require('./Campaign');
+
+var campaigns = [];
+
+exports.create = async function (key, title, campaigndetail) {
+    campaigns[key] = new Campaign(key, title, campaigndetail);
+    return campaigns[key];
+}
+
+exports.read = async function (key) {
+    return campaigns[key];  
+};
+
+exports.keyList = async function() { return Object.keys(campaigns); };
+
+exports.count = async function () { return campaigns.length; };
+
+exports.close = async function () {};
--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const campaigns = require('../models/campaign-memory');
 
 /* GET listing of all campaigns */
 router.get('/', function(req, res, next) {
     res.render('campaigns/index', { });
+});
+
+/* GET view a single campaign */
+router.get('/view', async function(req, res, next) {
+    var campaign = await campaigns.read(req.query.key);
+    res.render('campaigns/view', { title: campaign.title, campaignkey: campaign.key, campaigndetail: campaign.campaigndetail });
 });
 
 // GET to the add campaign form
@@ -14,6 +21,10 @@ router.get('/add', function(req, res, next) {
 // POST to add a new campaign
 router.post('/save', async (req, res, next) => {
     // Logic to save new campaign
+    var campaign;
+    campaign = await campaigns.create(req.body.campaignkey, req.body.title, req.body.campaigndetail);
+    res.redirect('/campaigns/view?key=' + req.body.campaignkey);
+
     console.log("Saving a new campaign.");
     res.send("Adding a new campaign.");
 });
